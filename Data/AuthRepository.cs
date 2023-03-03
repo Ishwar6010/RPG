b7804da700936977b87417c1568ae7c2d269c95b
Ishwar <110125203+Ishwar6010@users.noreply.github.com>
2023-03-03 18:19:35 +0530

    Add files via upload

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using RPGgame.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGgame.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _dataContext;
+        private readonly IConfiguration _configuration;
+        public AuthRepository(DataContext dataContext,IConfiguration configuration)
+        {
+            _dataContext = dataContext;
+            _configuration = configuration;
+        }
+
+        public async Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            ServiceResponse<string> response = new ServiceResponse<string>();
+            User user = await _dataContext.Users.FirstOrDefaultAsync(x => x.userName.ToLower().Equals(username.ToLower()));
+            if (user == null)
+            {
+                response.Success = false;
+                response.Message = "User Already Exists";
+                return response;
+            }
+            else if (!VerifyPasswordHash(password, user.passwordHash, user.passwordSalt))
+            {
+                response.Success = false;
+                response.Message = "User Already Exists";
+                return response;
+            }
+            else
+                response.Data = CreateToken(user);
+                //response.Data = user.Id.ToString();
+            return response;
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            if (await UserExists(user.userName))
+            {
+                response.Success = false;
+                response.Message = "User Already Exists";
+                return response;
+            }
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+            user.passwordHash = passwordHash;
+            user.passwordSalt = passwordSalt;
+            await _dataContext.Users.AddAsync(user);
+            await _dataContext.SaveChangesAsync();
+            response.Data = user.Id;
+            return response;
+
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _dataContext.Users.AnyAsync(x => x.userName.ToLower() == username.ToLower()));
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i=0;i<computedHash.Length;i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                        return false;
+                }
+            }
+            return true;
+        }
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString()),
+                new Claim(ClaimTypes.Name,user.userName),
+                new Claim(ClaimTypes.Role,user.Role)
+            };
+            SymmetricSecurityKey key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ Data/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RPGgame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPGgame.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<CharacterSkill> CharacterSkills { get; set; }
+
+        protected override void  OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CharacterSkill>().HasKey(cs => new { cs.CharacterId, cs.SkillId });
+
+            //setting default value of role as Player
+            modelBuilder.Entity<User>().Property(x => x.Role).HasDefaultValue("Player");
+        }
+    }
+}
