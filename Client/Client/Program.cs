951b5fcf0033cf59fe85c81aadee60970743076b
Ishwar6010 <guptaishwar6010@gmail.com>
2023-03-07 23:33:52 +0530

    Adding Unary grpc first commit

---
+++ Client/Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using System;
+
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:44307/");
+            var client = new GrpcPopulation.populationProvider.populationProviderClient(channel);
+            Console.WriteLine(client.getPopulation(new GrpcPopulation.populationRequest { State="WB"}));
+        }
+    }
+}
---
+++ Server/Server/populationProviderService.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+using GrpcPopulation;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class populationProviderService:populationProvider.populationProviderBase
+    {
+        public override Task<populationResponse> getPopulation(populationRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new populationResponse { Count = "100"});
+        }
+    }
+}
